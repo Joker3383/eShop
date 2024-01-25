b7830c74033c40d9a8912ca0793559d9b9293f5a
Максим Вовкович <vovkovihc542@gmail.com>
2024-01-25 14:54:44 +0200

    Catalog.API: configured AutoMappper

---
+++ src/Catalog/Catalog.API/Catalog.API/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Catalog.API.Data;
+using Catalog.API.Models.Dto;
+
+namespace Catalog.API.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Product, ProductDto>();
+        CreateMap<ProductDto, Product>();
+    }
+}
