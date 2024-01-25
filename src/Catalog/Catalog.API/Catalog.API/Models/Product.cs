f7711376a10cec519777f2b17c6325bd73ec09ba
Максим Вовкович <vovkovihc542@gmail.com>
2024-01-25 14:21:21 +0200

    .

---
+++ src/Catalog/Catalog.API/Catalog.API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Data;
+
+public class Product
+{
+    [Key] 
+    public int ProductId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Range(0, Double.MaxValue)]
+    public decimal Price { get; set; }
+    
+    public string CategoryName { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+}   
